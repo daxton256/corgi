1d5008fae4d2925505dc6178ee3ee1994600cc9a
daxton256 <technoboy493@gmail.com>
2025-02-11 23:11:52 -0800

    river

--- decoder.js
+++ decoder.js
@@ -1,54 +1,89 @@
+
 function randomString(length) {
-    var st = "";
-    for(var i = 0; i < length; i++) {
-      st += String.fromCharCode(randomNumber(65,90));
-    }
-    return st;
+  var st = "";
+  for(var i = 0; i < length; i++) {
+    st += String.fromCharCode(randomNumber(65,90));
   }
+  return st;
+}
+
+// a function that encodes strings into hexadecimal format.
+// string {string} - the string to be encoded
+function encodeHex(string) {
+  var hex = "";
+  for(var c = 0; c < string.length; c++){
+    hex += string.charCodeAt(c).toString(16); //convert char to hex
+  }
+  return hex;
+}
 
 //a function that allows for HTTP requests to any server and REST data.
 //onresponse {function} - a function that gets executed with the 'data" argument when the server responds.
-//data {JSON} - data to be sent to the server. 
-//  data.method {string} - the REST method to connect to the server with.
-//  data.ua {string} - the user agent sent to the server.
+//headers {JSON} - data to be sent to the server. 
 //cache {boolean} - decides if the content should be cached or not.
-function httpget(onresponse, data, cache){ 
-    //a function that allows for HTTP requests to any server.
-    //server side converts whatever string is needed into an image containing ASCII numbers
-    //client side draws the image out on a canvas and gets the pixel values, then converts them back into a string.
-    var st = Date.now();
-    var dp_id = randomString(16); //identifier for data reading canvas
-    var dl_id = randomString(16); //identifier for data retrieval canvas
-    var anti_cache;
-    if(cache) {
-      anti_cache = randomString(16);
-    } else {
-      anti_cache = "none";
-    }
-    //creating hidden data reading canvas
-    createCanvas(dp_id, 100, 100);
-    hideElement(dp_id);
-    setActiveCanvas(dp_id);
-    //creating hidden data retrieval image
-    image(dl_id, "https://00d6-72-193-9-27.ngrok-free.app/?data=" + data + "&ac=" + anti_cache);
-    hideElement(dl_id);
-    //checking if the image has loaded.
-    var loadChecker = setInterval(function(){
-    drawImage(dl_id, 0, 0, 100, 100); //draws the image on the canvas
-    if(getImageData(0, 0, 1, 1).data[0] != 0) { //runs when image data has loaded.
-    clearInterval(loadChecker); //clears the load checker interval when the image has loaded before proceeding
-    var id = getImageData(0, 0, 100, 100); //getting the entirety of image data
-    var string = ""; //creating a blank string for data retrieval
+function srvHTTP(onresponse, verbose, cache, url, method, data, headers){ 
+
+  var st = Date.now(); //start time for speed testing
+  var dp_id = randomString(16); //generating random id for loading canvas
+  var dl_id = randomString(16); //generating random id for downloading image
+  var res = 765; //image resoultion to match the server's response (res x res)
+  var anti_cache;
+
+  if(!cache) {
+    anti_cache = randomString(16); //by randomizing the cache parameter, the url will appear new to code.org's request server causing the request to be fresh data.
+  } else {
+    anti_cache = "none"; //by setting anti_cache to the "none" value the request will remain the same, causing code.org's request server to use the cached response (if any)
+  }
+
+  createCanvas(dp_id, res, res); //creating image retrieval canvas.
+  hideElement(dp_id); //hiding image retrieval canvas.
+  setActiveCanvas(dp_id); //setting image retrieval canvas to the main canvas.
   
-    for(var x = 0; x < 100*100; x+=4){ //looping through pixel data
-      if(id.data[x] != 0) { //checking if data is valid
-        string += String.fromCharCode(id.data[x]); //assemble the server response from the image's ASCII values.
+  var sd = encodeHex(JSON.stringify( //encoding request data into hexadecimal format for URL-safe data.
+      {
+        "url": url,
+        "method": method,
+        "data": data,
+        "headers": headers,
+        "ac": anti_cache
+      }
+    ));
+    
+  image(dl_id, "https://dbhs.dev/corgi/?data=" + sd); //sending image download request
+  hideElement(dl_id);
+  if(verbose) {console.log("Downloading data...");}
+  var loadChecker = setInterval(function(){
+    try{
+      drawImage(dl_id, 0, 0, res, res); 
+      if(getImageData(0, 0, 1, 1).data[0] != 0) {
+        clearInterval(loadChecker); //clears the load checker interval when the image has loaded before proceeding
+        var id = getImageData(0, 0, res, res); //getting the entirety of image data
+        var string = ""; //creating a blank string for data retrieval
+        if(verbose) {console.log("Image data fully loaded, reconstructing data.");}
+        for(var x = 4; x < res*res; x+=4){ //looping through pixel data
+          if(id.data[x-2] == 255 && id.data[x-3] == 0) { //if the current pixel is blue (0,0,255) that means the data has ended and there is no more need to traverse the pixels.
+            break; //break out of loop on blue end pixel.
+          } else {
+            string += String.fromCharCode(id.data[x-2]); //assemble the server response from the image's ASCII values.
+          }
+        }
+        if(verbose) { console.log("Data reconstructed, load finished in "+ (Date.now() - st) + " ms"); }
+        onresponse(string);
       }
     }
-    console.log("load finished in "+ (Date.now() - st) + " ms");
+    catch {
+      clearInterval(loadChecker);
+      if(verbose) { console.log("Request error."); }
+      return "request error.";
+    }
     deleteElement(dp_id); //clearing the temp canvas element
     deleteElement(dl_id); //clearing the temp image element
-    onresponse(string); //running the onresponse function with server response
-    }
-    }, 50);
-  }
+  }, 50);
+}
+
+//make a template string to easily include variables and other data in a string.
+//string {string} - the string to use with the template. to insert variables use ^^<var index>^^ eg. (^^1^^)
+//items {list} - a list of items to use within the template.
+function templateString(string, items) {
+  
+}
